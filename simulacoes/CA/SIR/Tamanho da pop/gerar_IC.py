eab3694b50d4d54c602d0e5c4cd495e46e59a452
Vvendet <froees147@gmail.com>
2025-05-07 21:14:31 -0300

    Estudos sobre tamanho da populacao

---
+++ simulacoes/CA/SIR/Tamanho da pop/gerar_IC.py
@@ -0,0 +1,24 @@
+import random
+
+numCells = 100
+CA = [[0 for x in range(numCells)] for y in range(numCells)]
+for i in range(numCells):
+	for j in range(numCells):
+		if random.randint(1,100) < 12:
+			CA[i][j] = 1
+		else:
+			CA[i][j] = 0
+
+print(CA)
+
+I = 0
+S = 0
+R = 0
+for i in range(numCells): 
+    for j in range(numCells):
+        if CA[i][j] == 1:
+            I += 1
+        elif CA[i][j] == 0:
+            S +=1
+
+print(I)
